85e43fd9f8d6b56a59770e0fe49f29d0df66bed6
Camila F. Sanchez Molina <cami.key982@gmail.com>
2025-11-30 22:02:16 -0300

    correccion de tp8- faltaba en los models definir la imlementacion de unos metodos

--- Models/Presupuesto.cs
+++ Models/Presupuesto.cs
@@ -2,23 +2,34 @@ namespace DistribuidoraInsumosMVC.Models
 {
     public class Presupuesto
     {
+        private const double IVA = 0.21;
         public int idPresupuesto { get; set; }
         public string nombreDestinatario { get; set; }
         public DateTime fechaCreacion { get; set; }
         public List<PresupuestoDetalle> detalle { get; set; }
 
-        public int MontoPresupuesto()
+        public double MontoPresupuesto()
         {
-            return 0;
+            double monto = 0;
+            foreach (var item in detalle)
+            {
+                monto += item.producto.precio * item.cantidad;
+            }
+            return monto;
         }
-        public int MontoPresupuestoConIva() //considerar iva 21 
+        public double MontoPresupuestoConIva()
         {
-            return 1;
+            return MontoPresupuesto()*IVA;
         } 
         
-        public int CantidadProductos() //contar total de productos (sumador de todas las cantidades del detalle)
+        public int CantidadProductos()
         {
-            return 2;
+            int cant = 0;
+            foreach (var item in detalle)
+            {
+                cant += item.cantidad;
+            }
+            return cant;
         }
     }
-}   
+} 
